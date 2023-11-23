d7e63383a70ad1e6734771ae43c32d3bbf282303
Jakub Pokrywka <jakubpokrywka@gmail.com>
2023-11-23 13:36:23 +0000

    start

---
+++ 1.py
@@ -0,0 +1,138 @@
+
+import evaluate
+import numpy as np
+from datasets import load_dataset, Sequence,ClassLabel
+from transformers import DataCollatorForTokenClassification
+from transformers import AutoTokenizer
+from transformers import pipeline
+from transformers import AutoModelForTokenClassification, TrainingArguments, Trainer
+import os 
+
+os.environ['WANDB_DISABLED'] = 'true'
+
+
+def tokenize_and_align_labels(examples):
+    tokenized_inputs = tokenizer(examples["tokens"], truncation=True, is_split_into_words=True)
+
+    labels = []
+    for i, label in enumerate(examples[f"ner_tags"]):
+        word_ids = tokenized_inputs.word_ids(batch_index=i)  # Map tokens to their respective word.
+        previous_word_idx = None
+        label_ids = []
+        for word_idx in word_ids:  # Set the special tokens to -100.
+            if word_idx is None:
+                label_ids.append(-100)
+            elif word_idx != previous_word_idx:  # Only label the first token of a given word.
+                label_ids.append(label[word_idx])
+            else:
+                label_ids.append(-100)
+            previous_word_idx = word_idx
+        labels.append(label_ids)
+
+    tokenized_inputs["labels"] = labels
+    return tokenized_inputs
+
+
+def compute_metrics(p):
+    predictions, labels = p
+    predictions = np.argmax(predictions, axis=2)
+
+    true_predictions = [
+        [label_list[p] for (p, l) in zip(prediction, label) if l != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+    true_labels = [
+        [label_list[l] for (p, l) in zip(prediction, label) if l != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+
+    results = seqeval.compute(predictions=true_predictions, references=true_labels)
+    return {
+        "precision": results["overall_precision"],
+        "recall": results["overall_recall"],
+        "f1": results["overall_f1"],
+        "accuracy": results["overall_accuracy"],
+    }
+
+
+
+id2label = {
+    0: "O",
+    1: "1",
+}
+label2id = {
+    "O": 0,
+    "1": 1,
+}
+
+
+
+
+
+
+from datasets import Dataset
+
+
+wnut = load_dataset("wnut_17")
+
+ds = Dataset.from_dict({"tokens": [['a','b','c'], ['a','e']], "ner_tags": [['0','0','1'], ['1','0']]})
+#ds.features['ner_tags'].feature.names = ['0','1']
+ner_tags_class_label = Sequence(ClassLabel(num_classes=2, names = ['0','1']))
+ds  = ds.cast_column('ner_tags', ner_tags_class_label)
+ds = ds.train_test_split()
+wnut = ds
+
+#label_list = wnut["train"].features[f"ner_tags"].feature.names
+label_list = ['0','1']
+
+
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+
+example = wnut["train"][0]
+tokenized_input = tokenizer(example["tokens"], is_split_into_words=True)
+tokens = tokenizer.convert_ids_to_tokens(tokenized_input["input_ids"])
+
+
+tokenized_wnut = wnut.map(tokenize_and_align_labels, batched=True)
+
+data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
+
+seqeval = evaluate.load("seqeval")
+
+
+labels = [label_list[i] for i in example[f"ner_tags"]]
+
+model = AutoModelForTokenClassification.from_pretrained(
+    "distilbert-base-uncased", num_labels=2, id2label=id2label, label2id=label2id
+)
+
+training_args = TrainingArguments(
+    output_dir="my_awesome_wnut_model",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=2,
+    weight_decay=0.01,
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+    report_to=None
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_wnut["train"],
+    eval_dataset=tokenized_wnut["test"],
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+)
+
+
+trainer.train()
+
+text = "The Golden State Warriors are an American professional basketball team based in San Francisco."
+
+classifier = pipeline("ner", model="stevhliu/my_awesome_wnut_model")
+classifier(text)
